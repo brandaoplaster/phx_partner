1975bbed19eef0928af3e49393a95051517aa712
brandaoplaster <brandaoplaster@gmail.com>
2022-11-29 23:23:52 -0300

    feat: Adding partner context

---
+++ lib/phx_partner/partners.ex
@@ -0,0 +1,71 @@
+defmodule PhxPartner.Partners do
+  @moduledoc """
+  The Partners Context
+  """
+
+  import Ecto.Query, warn: false
+  alias PhxPartner.Repo
+  alias PhxPartner.Partners.Partner
+
+  @doc """
+  Returns the list of partners.
+
+  ## Examples
+
+      iex> list_partners()
+      [%Partner{}, ...]
+
+  """
+  def list_partners do
+    Repo.all(Partner)
+  end
+
+  @doc """
+  Creates a partner.
+
+  ## Examples
+
+      iex> create_partner(%{field: value})
+      {:ok, %Partner{}}
+
+      iex> create_partner(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_partner(attrs \\ %{}) do
+    %Partner{}
+    |> Partner.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a partner.
+
+  ## Examples
+
+      iex> update_partner(partner, %{field: new_value})
+      {:ok, %Partner{}}
+
+      iex> update_partner(partner, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_partner(%Partner{} = partner, attrs) do
+    partner
+    |> Partner.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking partner changes.
+
+  ## Examples
+
+      iex> change_partner(partner)
+      %Ecto.Changeset{data: %Partner{}}
+
+  """
+  def change_partner(%Partner{} = partner, attrs \\ %{}) do
+    Partner.changeset(partner, attrs)
+  end
+end
